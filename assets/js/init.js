d2fa0365c90e5bc36cd937fcb58ffe7d5a9bcef3
Vitalii A <crazyyy@gmail.com>
2017-12-11 18:49:39 +0200

    bf gulp fired

---
+++ assets/js/init.js
@@ -0,0 +1,230 @@
+var initPhotoSwipe = function(gallerySelector, itemSelector, linkSelector, captionSelector) {
+
+  var isEmpty = function(val) {
+    return (val === undefined || val == null || val.length <= 0) ? true : false;
+  }
+
+  var parseThumbnailElements = function(el) {
+    var thumbElements = el.querySelectorAll(itemSelector),
+      numNodes = thumbElements.length,
+      items = [],
+      galleryItemEl,
+      linkEl,
+      size,
+      item;
+    for (var i = 0; i < numNodes; i++) {
+
+      galleryItemEl = thumbElements[i];
+
+      if (galleryItemEl.nodeType !== 1) {
+        continue;
+      }
+
+      linkEl = galleryItemEl.querySelector(linkSelector);
+
+      if (linkEl) {
+        if (linkEl.hasAttribute('data-size')) {
+          size = linkEl.getAttribute('data-size').split('x');
+
+          item = {
+            src: linkEl.getAttribute('href'),
+            w: parseInt(size[0], 10),
+            h: parseInt(size[1], 10)
+          };
+
+          if (!isEmpty(captionSelector)) {
+            if (galleryItemEl.querySelector(captionSelector) != null) {
+              item.title = galleryItemEl.querySelector(captionSelector).innerHTML;
+            }
+          }
+
+          if (linkEl.querySelector("IMG") != null) {
+            item.msrc = linkEl.querySelector("IMG").getAttribute('src');
+          }
+
+          item.el = galleryItemEl;
+          items.push(item);
+        }
+      }
+    }
+
+    return items;
+  };
+
+  var closest = function closest(el, fn) {
+    return el && (fn(el) ? el : closest(el.parentNode, fn));
+  };
+
+  var onThumbnailsClick = function(e) {
+    e = e || window.event;
+    e.preventDefault ? e.preventDefault() : e.returnValue = false;
+
+    var eTarget = e.target || e.srcElement;
+
+    var clickedList = closest(eTarget, function(el) {
+      return (el.tagName && ((' ' + el.className + ' ').indexOf(' ' + gallerySelector.replace(".", "") + ' ') > -1));
+    });
+    var clickedListItem = closest(eTarget, function(el) {
+      return (el.tagName && ((' ' + el.className + ' ').indexOf(' ' + itemSelector.replace(".", "") + ' ') > -1));
+    });
+
+    if (!clickedListItem) {
+      return;
+    }
+
+    var clickedGallery = clickedList,
+      childNodes = clickedList.querySelectorAll(itemSelector),
+      numChildNodes = clickedList.querySelectorAll(itemSelector).length,
+      nodeIndex = 0,
+      index;
+
+    for (var i = 0; i < numChildNodes; i++) {
+      if (childNodes[i].nodeType !== 1) {
+        continue;
+      }
+
+      if (childNodes[i] === clickedListItem) {
+        index = nodeIndex;
+        break;
+      }
+      nodeIndex++;
+    }
+
+    if (index >= 0) {
+      openPhotoSwipe(index, clickedGallery);
+    }
+    return false;
+  };
+
+  var photoswipeParseHash = function() {
+    var hash = window.location.hash.substring(1),
+      params = {};
+
+    if (hash.length < 5) {
+      return params;
+    }
+
+    var vars = hash.split('&');
+    for (var i = 0; i < vars.length; i++) {
+      if (!vars[i]) {
+        continue;
+      }
+      var pair = vars[i].split('=');
+      if (pair.length < 2) {
+        continue;
+      }
+      params[pair[0]] = pair[1];
+    }
+
+    if (params.gid) {
+      params.gid = parseInt(params.gid, 10);
+    }
+
+    return params;
+  };
+
+  var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
+    var pswpElement = document.querySelectorAll('.pswp')[0],
+      gallery,
+      options,
+      items;
+
+    items = parseThumbnailElements(galleryElement);
+
+    options = {
+      galleryUID: galleryElement.getAttribute('data-pswp-uid'),
+      getThumbBoundsFn: function(index) {
+        var thumbnail = items[index].el.getElementsByTagName('img')[0],
+          pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
+          rect = thumbnail.getBoundingClientRect();
+
+        return {
+          x: rect.left,
+          y: rect.top + pageYScroll,
+          w: rect.width
+        };
+      },
+      bgOpacity: 0.9
+
+    };
+
+    if (fromURL) {
+      if (options.galleryPIDs) {
+        for (var j = 0; j < items.length; j++) {
+          if (items[j].pid == index) {
+            options.index = j;
+            break;
+          }
+        }
+      } else {
+        options.index = parseInt(index, 10) - 1;
+      }
+    } else {
+      options.index = parseInt(index, 10);
+    }
+
+    if (isNaN(options.index)) {
+      return;
+    }
+
+    if (disableAnimation) {
+      options.showAnimationDuration = 0;
+    }
+
+    gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+  };
+
+  if (document.getElementsByClassName(gallerySelector) !== null) {
+
+    document.getElementsByTagName('body')[0].insertAdjacentHTML("beforeend", "\
+<div class=\"pswp\" tabindex=\"-1\" role=\"dialog\" aria-hidden=\"true\">\
+  <div class=\"pswp__bg\"></div>\
+  <div class=\"pswp__scroll-wrap\">\
+    <div class=\"pswp__container\">\
+      <div class=\"pswp__item\"></div>\
+      <div class=\"pswp__item\"></div>\
+      <div class=\"pswp__item\"></div>\
+    </div>\
+    <div class=\"pswp__ui pswp__ui--hidden\">\
+      <div class=\"pswp__top-bar\">\
+        <div class=\"pswp__counter\"></div>\
+        <button class=\"pswp__button pswp__button--close\" title=\"Close (Esc)\"></button>\
+        <button class=\"pswp__button pswp__button--share\" title=\"Share\"></button>\
+        <button class=\"pswp__button pswp__button--fs\" title=\"Toggle fullscreen\"></button>\
+        <button class=\"pswp__button pswp__button--zoom\" title=\"Zoom in/out\"></button>\
+        <div class=\"pswp__preloader\">\
+          <div class=\"pswp__preloader__icn\">\
+            <div class=\"pswp__preloader__cut\">\
+              <div class=\"pswp__preloader__donut\"></div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div class=\"pswp__share-modal pswp__share-modal--hidden pswp__single-tap\">\
+        <div class=\"pswp__share-tooltip\"></div>\
+      </div>\
+      <button class=\"pswp__button pswp__button--arrow--left\" title=\"Previous (arrow left)\">\
+      </button>\
+      <button class=\"pswp__button pswp__button--arrow--right\" title=\"Next (arrow right)\">\
+      </button>\
+      <div class=\"pswp__caption\">\
+        <div class=\"pswp__caption__center\"></div>\
+      </div>\
+    </div>\
+  </div>\
+</div>");
+
+    var galleryElements = document.querySelectorAll(gallerySelector);
+
+    for (var i = 0, l = galleryElements.length; i < l; i++) {
+      galleryElements[i].setAttribute('data-pswp-uid', i + 1);
+      galleryElements[i].onclick = onThumbnailsClick;
+    }
+
+    var hashData = photoswipeParseHash();
+    if (hashData.pid && hashData.gid) {
+      openPhotoSwipe(hashData.pid, galleryElements[hashData.gid - 1], true, true);
+    }
+  }
+};
